c53ef58ca0ecff38b114cc5ea8995fbafbc26692
achinup <achinthyau@gmail.com>
2024-12-03 23:01:23 +0530

    hre

--- src/app/Nevbar.js
+++ src/app/Nevbar.js
@@ -74,7 +74,7 @@ const Nevbar = () => {
       }}
     >
       <div className="container-fluid">
-        <Link href="/" className="navbar-brand">Raithapi</Link>
+        <Link href="/" passHref className="navbar-brand">Raithapi</Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -89,7 +89,7 @@ const Nevbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link href="/" className="nav-link active" aria-current="page">
+              <Link href="/" passHref className="nav-link active" aria-current="page">
                 Home
               </Link>
             </li>
